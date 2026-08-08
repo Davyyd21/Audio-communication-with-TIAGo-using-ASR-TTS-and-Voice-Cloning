c686531b2c4d1d3830c3968d06f6bd2490d86dff
David_Munteanu <davmunteanu21@gmail.com>
2026-08-08 15:15:56 +0300

    Refactor LaboratoryState class formatting

--- tiago_assistant/laboratory_state.py
+++ tiago_assistant/laboratory_state.py
@@ -3,20 +3,11 @@ class LaboratoryState:
     def __init__(self):
         self.active_laboratory = None
 
-
-    def set_active_laboratory(
-        self,
-        laboratory_name: str,
-    ) -> None:
-
+    def set_active_laboratory(self,laboratory_name: str,)->None:
         self.active_laboratory = laboratory_name
 
-
     def get_active_laboratory(self):
-
         return self.active_laboratory
 
-
     def clear(self) -> None:
-
         self.active_laboratory = None
