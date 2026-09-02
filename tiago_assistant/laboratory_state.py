70bf8a81155714e3dec3213bb8938fc0cdfc9bde
David_Munteanu <davmunteanu21@gmail.com>
2026-09-02 11:37:02 +0300

    Refactor LaboratoryState class with comments

--- tiago_assistant/laboratory_state.py
+++ tiago_assistant/laboratory_state.py
@@ -1,22 +1,13 @@
 class LaboratoryState:
-
+    # tine minte ce laborator e "activ" in conversatia curenta, ca sa stim la ce sa raportam intrebarile urmatoare
     def __init__(self):
         self.active_laboratory = None
 
-
-    def set_active_laboratory(
-        self,
-        laboratory_name: str,
-    ) -> None:
-
+    def set_active_laboratory(self, laboratory_name: str) -> None:
         self.active_laboratory = laboratory_name
 
-
     def get_active_laboratory(self):
-
         return self.active_laboratory
 
-
     def clear(self) -> None:
-
         self.active_laboratory = None
